8c93e63c3ac1764e89dd89a1ed8a9410730705e3
Jeanette S <nova777nova@hotmail.com>
2024-05-29 15:56:26 +0200

    Part 2 done

--- src/main/java/se/lexicon/springbootproject/entity/AppUser.java
+++ src/main/java/se/lexicon/springbootproject/entity/AppUser.java
@@ -11,32 +11,30 @@ import java.time.LocalDate;
 @EqualsAndHashCode
 @ToString
 
-
 @Entity
 public class AppUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column (nullable = false, unique = true)
-    private String username;
+    @Column(nullable = false,unique = true)
+    @Setter private String username;
 
-    @Column (nullable = false)
-    private String password;
+    @Column(nullable = false)
+    @Setter private String password;
 
     @Column
-    @Setter
-    private LocalDate regDate;
+    @Setter private LocalDate regDate;
 
     @Setter
     @OneToOne
-    @JoinColumn (name = "details_id")
+    @JoinColumn(name = "details_id")
     private Details userDetails;
 
     public AppUser(String username, String password) {
-        this.id = id;
         this.username = username;
         this.password = password;
         this.regDate = LocalDate.now();
+
     }
 }
--- src/main/java/se/lexicon/springbootproject/entity/Details.java
+++ src/main/java/se/lexicon/springbootproject/entity/Details.java
@@ -1,37 +1,34 @@
 package se.lexicon.springbootproject.entity;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
+
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 @ToString
 
 @Entity
 public class Details {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable = false, length = 100)
-    @Setter
-    private String name;
+    @Column(nullable = false,length = 100)
+    @Setter private String name;
 
-    @Column (nullable = false, length = 100, unique = true)
-    @Setter
-    private String email;
+    @Column(nullable = false,length = 100, unique = true)
+    @Setter private String email;
 
     @Column
-    @Setter
-    private LocalDate birthDate;
+    @Setter private LocalDate birthdate;
 
-    public Details(String name, String email, LocalDate birthDate) {
+    public Details(String name, String email, LocalDate birthdate) {
         this.name = name;
         this.email = email;
-        this.birthDate = birthDate;
+        this.birthdate = birthdate;
     }
 }

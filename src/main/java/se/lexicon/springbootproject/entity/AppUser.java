1089ed33dd3ee6864d0d45ed2408b12531168737
Jeanette S <nova777nova@hotmail.com>
2024-06-05 12:37:08 +0200

    added comments in the code

--- src/main/java/se/lexicon/springbootproject/entity/AppUser.java
+++ src/main/java/se/lexicon/springbootproject/entity/AppUser.java
@@ -14,25 +14,33 @@ import java.util.List;
 
 @Entity
 public class AppUser {
+    // ID of user
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false)
     private int id;
 
+
+    // Username of user
     @Column(nullable = false,unique = true)
     @Setter private String username;
 
+    // Password of user
     @Column(nullable = false)
     @Setter private String password;
 
+    // Regdate of user
     @Column
     @Setter private LocalDate regDate;
 
+    // User deatils
     @Setter
     @OneToOne (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinColumn(name = "details_id")
     private Details userDetails;
 
+
+    //List of books
     @Setter
     @ManyToMany
     @JoinTable(name = "appuser_book",
@@ -40,26 +48,32 @@ public class AppUser {
             inverseJoinColumns = @JoinColumn(name = "book_id"))
     private List<Book> books;
 
+
+    // List of books loans
     @OneToMany(mappedBy = "borrower")
     private List<BookLoan> loans;
 
+
+    // list of books on loan to user
     @OneToMany (mappedBy = "borrower")
     private List<Book> bookLoans;
 
+
+    // create a new user with username and password
     public AppUser(String username, String password) {
         this.username = username;
         this.password = password;
         this.regDate = LocalDate.now();
 
     }
-
+    // add a book loan to user
     public void addBookLoan(BookLoan bookLoan) {
         bookLoan.setBorrower(this);
         loans.add(bookLoan);
     }
 
 
-
+  // Method to add a book loan to the user, ensuring the book is available
     public void addBookLoan(Book book) {
         if (!book.isAvailable()) {
             throw new IllegalStateException("Book is not available for loan.");

7a49c8fa7f6a3180a3b249669c4a6b0f5996dafb
Jeanette S <nova777nova@hotmail.com>
2024-06-05 13:20:08 +0200

    added comments in the code

--- src/main/java/se/lexicon/springbootproject/entity/Book.java
+++ src/main/java/se/lexicon/springbootproject/entity/Book.java
@@ -16,46 +16,54 @@ import java.util.Set;
 
 @Entity
 public class Book {
-
+// Id of the book
     @Id
     @GeneratedValue
     private int id;
 
-
+//ISBN
     @Column (nullable = false, length = 13)
     @Setter
     private String isbn;
 
-
+//Book title
     @Column (nullable = false)
     @Setter
     private String Title;
 
+
+//set authors associated with book
     @ManyToMany
     @JoinTable(name = "book_authors",
             joinColumns = @JoinColumn(name = "book_id"),
             inverseJoinColumns = @JoinColumn(name = "author_id"))
     @Setter private Set<Author> authors;
 
-
+// maximum days a book can be loaned
     @Column (nullable = false)
     private int maxLoanDays;
 
+
+//List of book loans
     @OneToMany (mappedBy = "book")
     @Setter
     private List<BookLoan> loans ;
 
+
+// Book constructor
     public Book(String isbn, String title, int maxLoanDays) {
         this.isbn = isbn;
         Title = title;
         this.maxLoanDays = maxLoanDays;
     }
 
+// set author
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "author_id")
     @Setter
     private Author author;
 
+// add and remove author
     public void addAuthor(Author author) {
         this.author = author;
     }
@@ -64,12 +72,18 @@ public class Book {
 
     }
 
+// calculate due date
     private LocalDate dueDate;
+
+    //book availability
     private boolean available;
 
+//set due date maximum loan days
     public void calculateDueDate() {
         dueDate = LocalDate.now().plusDays(maxLoanDays);
     }
+
+    //set availability
     public void setAvailable(boolean available) {
         this.available = available;
     }
